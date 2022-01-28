309017475892798985858833f5cd55fe40808081
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-01-27 16:20:56 -0800

    my test runs clearly now. Made sure the string was equal to to the actual and expected

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 export function makeStatsString(headCount, middleCount, bottomCount) {
-    return ` I selected the head ${headCount} times, the middle ${middleCount} times and the bottom ${bottomCount} times`;
+    return `You clicked on the head ${headCount} times, on the middle ${middleCount} times, and on the bottom ${bottomCount} time. And everybody loves your character's catchphrases:`;
 }
